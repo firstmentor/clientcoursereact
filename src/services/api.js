a5885c83354d65caef052da740c6f5839bca36da
firstmentor <jainvikas887@gmail.com>
2025-07-23 01:33:34 +0530

    dghdfgfffghf

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://servercourseapi.onrender.com/api',
   withCredentials: true, // for cookies
 });
 
